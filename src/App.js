8df2caf76b127bb0316b0a04928a2b8d98aa8680
Robert Santos <rssp_96@hotmail.com>
2022-06-21 10:48:41 -0500

    main page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
-import React from 'react'
+import "./";
+import axios from "axios";
+import { React, useEffect, useState } from "react";
+import Carrousel from "./Components/Carrousel";
+import Navbar from "./Components/Navbar";
+import styled from "styled-components";
+import Description from "./Components/Description";
+
+const Home = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+
+  @media (max-width: 520px) {
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 50px;
+    align-items: center;
+    justify-content: center;
+    padding-left: 10%;
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 12px;
+  font-weight: 800;
+  padding-top: 70px;
+  color: #dedede;
+  margin-left: 6%;
+  margin-bottom: 15px;
+
+  & > span {
+    font-weight: 900;
+    color: #616161;
+  }
+`;
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
-}
+  const [product, setProduct] = useState({});
+  useEffect(() => {
+    axios
+      .get(
+        "https://graditest-store.myshopify.com/products/free-trainer-3-mmw.js"
+      )
+      .then(({ data }) => setProduct(data));
+  }, []);
 
+  return (
+    <>
+      <Navbar />
+      <Title>
+        Catalog / Snakers / <span>{product.title}</span>{" "}
+      </Title>
+      <Home>
+        <Carrousel product={product} />
+        <Description product={product} />
+      </Home>
+    </>
+  );
+}
